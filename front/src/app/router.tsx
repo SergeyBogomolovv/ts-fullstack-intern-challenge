050a4453246681a9984ee18a0d454a31b7d51f23
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-09-04 19:26:53 +0300

    feat(front): add auth pages

--- front/src/app/router.tsx
+++ front/src/app/router.tsx
@@ -6,12 +6,19 @@ import {
 import RootLayout from "./root-layout";
 import HomePage from "../pages/home";
 import FavoritesPage from "../pages/favorites";
+import AuthLayout from "./auth-layout";
+import SignInPage from "@/pages/sign-in";
+import SignUpPage from "@/pages/sign-up";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<RootLayout />}>
       <Route path="/" element={<HomePage />} />
       <Route path="favorites" element={<FavoritesPage />} />
+      <Route element={<AuthLayout />}>
+        <Route path="sign-in" element={<SignInPage />} />
+        <Route path="sign-up" element={<SignUpPage />} />
+      </Route>
     </Route>,
   ),
 );
