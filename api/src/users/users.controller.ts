b180bbec11b095dfdfe9ba39a44ff2611cded158
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-09-05 10:01:03 +0300

    feat: add create user method

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AddUserDto } from './dto/add-user.dto';
 
-@Controller('users')
+@Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+  @Post()
+  async createUser(@Body() dto: AddUserDto) {
+    return this.usersService.create(dto);
+  }
 }
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
+import { AddUserDto } from './dto/add-user.dto';
+import bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private usersRepository: Repository<UserEntity>,
+  ) {}
+
+  async create(dto: AddUserDto) {
+    const existingUser = await this.usersRepository.findOneBy({
+      login: dto.login,
+    });
+    if (existingUser) {
+      throw new ConflictException('User already exists');
+    }
+    const hashedPassword = await this.hash(dto.password);
+    return this.usersRepository.save({ ...dto, password: hashedPassword });
+  }
+
+  private hash(payload: string): Promise<string> {
+    return bcrypt.hash(payload, bcrypt.genSaltSync());
+  }
+
+  private compare(payload: string, hashed: string): Promise<boolean> {
+    return bcrypt.compare(payload, hashed);
+  }
+}
