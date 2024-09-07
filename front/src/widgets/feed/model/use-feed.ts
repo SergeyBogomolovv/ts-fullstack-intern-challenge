818f797218ad7cc6e35566d46bda8ffe51870b29
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-09-07 16:11:18 +0300

    feat: add favorites check in feed

--- front/src/widgets/feed/model/use-feed.ts
+++ front/src/widgets/feed/model/use-feed.ts
@@ -1,8 +1,15 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { $cats } from "@/shared/config/axios";
 import { useEffect } from "react";
-import { Cat, CatImage, CatImageSchema } from "@/shared/schemas";
+import {
+  Cat,
+  CatImage,
+  CatImageSchema,
+  Favourite,
+  FavouriteSchema,
+} from "@/shared/schemas";
 import { z } from "zod";
+import getUser from "@/shared/utils/get-user";
 
 export const useFeed = () => {
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
@@ -10,16 +17,34 @@ export const useFeed = () => {
       queryKey: ["feed"],
       queryFn: async ({ pageParam = 0 }): Promise<Cat[]> => {
         const { data } = await $cats.get<CatImage[]>(
-          `/images/search?size=med&mime_types=jpg&format=json&order=RANDOM&page=${pageParam}&limit=15`,
+          `/images/search?size=med&mime_types=jpg&format=json&order=DESC&page=${pageParam}&limit=15`,
         );
-        const { success } = z.array(CatImageSchema).safeParse(data);
 
+        const { success } = z.array(CatImageSchema).safeParse(data);
         if (success) {
-          return data.map((cat) => ({
+          const user = getUser();
+          const cats = data.map((cat) => ({
             cat_id: cat.id,
             image_url: cat.url,
-            favorite: false, //TODO: set it right
+            favorite: false,
           }));
+
+          if (user) {
+            const { data: favorites } = await $cats.get<Favourite[]>(
+              `/favourites?sub_id=${user?.login}&size=med&mime_types=jpg&format=json&order=DESC&page=${pageParam}&limit=15`,
+            );
+            const { success } = z.array(FavouriteSchema).safeParse(favorites);
+            if (success) {
+              for (const cat of favorites) {
+                const index = cats.findIndex((c) => c.cat_id === cat.image_id);
+                if (index !== -1) {
+                  cats[index].favorite = true;
+                }
+              }
+            }
+          }
+
+          return cats;
         }
         return [];
       },
