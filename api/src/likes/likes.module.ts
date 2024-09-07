ae2029b9c92dd2df7baae1e8c19887ea25494cc1
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-09-07 11:47:58 +0300

    feat: add to favourites in cats api and backend

--- api/src/likes/likes.module.ts
+++ api/src/likes/likes.module.ts
@@ -4,9 +4,24 @@ import { LikesController } from './likes.controller';
 import { UsersModule } from 'src/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LikeEntity } from './entities/like.entity';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forFeature([LikeEntity])],
+  imports: [
+    HttpModule.registerAsync({
+      useFactory: (config: ConfigService) => ({
+        baseURL: 'https://api.thecatapi.com/v1',
+        headers: {
+          'x-api-key': config.get('CAT_API_KEY'),
+        },
+      }),
+      imports: [ConfigModule],
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    TypeOrmModule.forFeature([LikeEntity]),
+  ],
   controllers: [LikesController],
   providers: [LikesService],
 })
