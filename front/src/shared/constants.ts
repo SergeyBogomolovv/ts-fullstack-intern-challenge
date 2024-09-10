805dd6bc5d2e5ebc268f08ea199550e40c0c41b7
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-09-10 12:31:54 +0300

    build(front): remove vars from constants

--- front/src/shared/constants.ts
+++ front/src/shared/constants.ts
@@ -1,5 +1,3 @@
-export const CATS_API_KEY =
-  import.meta.env.VITE_CATS_API_KEY ||
-  "live_ie8Toulv441tyBHwi2gSyIeVA04anu9FKRE4JI6otJcPH7ZLMlU156Prer9xtbeV";
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+export const CATS_API_KEY = import.meta.env.VITE_CATS_API_KEY;
+export const API_URL = import.meta.env.VITE_API_URL;
 export const ACCESS_TOKEN_KEY = "access_token";
